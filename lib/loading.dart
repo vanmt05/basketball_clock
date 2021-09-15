8fbd9b3293296f506b89ac7a68109d6a649cf8b7
vanmt05 <ivan.danielmt0512@outlook.com>
2021-09-15 16:44:09 -0400

    Reverted changes of control_buttons.dart and implement reload page.

--- lib/loading.dart
+++ lib/loading.dart
@@ -5,11 +5,10 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.brown[100],
       child: Center(
         child: SpinKitChasingDots(
-          color: Colors.brown,
-          size: 50.0,
+          color: Colors.black,
+          size: 150.0,
         ),
       ),
     );
