5bb15782a88facc990512f431fd5cf96fa172f27
Ivan <ivan.danielmt0512@outlook.com>
2022-02-09 13:24:45 -0400

    Change splash screen in Android files (only) directly without a package

--- lib/main.dart
+++ lib/main.dart
@@ -25,22 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Buttons with Icon'),
+      home: Home(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Home();
   }
 }
